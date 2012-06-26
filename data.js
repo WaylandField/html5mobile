7075d4056cc82a602c91affba528ae1dd8bb8ea3
Yichang <tian.yichang@gmail.com>
2012-06-26 10:06:09 +0800

    canvas

---
+++ data.js
@@ -0,0 +1,9 @@
+var roles=[
+{name:"Dev Manager",count:"8",id:4},
+{name:"Achitect",count:"6",id:3},
+{name:"CEO",count:"4",id:1}
+{name:"CTO",count:"3",id:1},
+{name:"Senior Software Engineer",count:"4",id:2},
+{name:"Product Manager",count:"1",id:5},
+{name:"programmer",count:"1",id:1},
+];
