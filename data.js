61c17b54bd959309fcc0aaae285807cf695f2a2c
Yichang <tian.yichang@gmail.com>
2012-06-27 16:27:06 +0800

    canvas html

--- data.js
+++ data.js
@@ -1,9 +1,33 @@
 var roles=[
-{name:"Dev Manager",count:"8",id:4},
-{name:"Achitect",count:"6",id:3},
-{name:"CEO",count:"4",id:1}
-{name:"CTO",count:"3",id:1},
+{name:"Dev Manager",count:"18",id:4},
+{name:"Achitect",count:"10",id:3},
+{name:"CEO",count:"4",id:1},
 {name:"Senior Software Engineer",count:"4",id:2},
-{name:"Product Manager",count:"1",id:5},
-{name:"programmer",count:"1",id:1},
+{name:"CTO",count:"3",id:1},
+{name:"programmer",count:"1",id:1}
 ];
+var groups = [
+              [{name:"Dev Manager",count:"46",id:4}],
+              [{name:"Product Manager",count:"38",id:5},
+               {name:"Senior Software Engineer",count:"30",id:7},
+               {name:"Senior Sales",count:"32",id:7}],
+              [{name:"CEO",count:"28",id:1},
+               {name:"Sales Manager",count:"22",id:7},
+               {name:"Achitect",count:"21",id:3},
+               {name:"CTO",count:"23",id:2},
+               {name:"Senior QA",count:"22",id:7},
+               {name:"QA Manager",count:"22",id:7}],
+              [{name:"Sales",count:"12",id:6},
+               {name:"Senior Customer Support Expert",count:"19",id:7},
+               {name:"DBA",count:"16",id:7},
+               {name:"Office Admin",count:"17",id:7},
+               {name:"IT Export",count:"11",id:7},
+               {name:"Product Service Expert",count:"35",id:7}],
+              [{name:"programmer",count:"9",id:8},
+               {name:"Sales",count:"8",id:7},
+               {name:"UI Developer",count:"8",id:7},
+               {name:"Java Developer",count:"2",id:7},
+               {name:"Project owner",count:"1",id:7}]
+              ];
+var laddler = [90, 60, 40,27,16];
+;
